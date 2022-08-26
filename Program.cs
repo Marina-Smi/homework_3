b7084eb63f8a73adfd9f9c98c784e06053b7c63f
Marina90 <miss.honeymari@gmail.com>
2022-08-26 08:08:52 +0300

    all zadachi

--- Program.cs
+++ Program.cs
@@ -54,24 +54,10 @@ Console.WriteLine($"Длина отрезка  {segmentLength}");
 //Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 
-Console.Write("Введите число: ");
-int cube = Convert.ToInt32(Console.ReadLine());
-
-void Cube(int[] cube)
-{
-  int count = 0;
-  int length = cube.Length;
-  while (count <  length){
-    cube[count] = Convert.ToInt32(Math.Pow(count, 3));
-    count++;
-  }
-}
-
-void PrintArry(int[] coll){
-  int count = coll.Length;
-  int index = 0;
-  while(index < count){
-    Console.Write(coll[index]+ " ");
-    index++;
-  }
-} 
+    {
+        Console.WriteLine("Введите число N");
+        int num = int.Parse(Console.ReadLine());
+        for (int ind = 1; ind <= num; ind++)
+            Console.WriteLine("{0}^2 = {1}", ind, ind*ind);
+        Console.ReadKey(true);
+    }
